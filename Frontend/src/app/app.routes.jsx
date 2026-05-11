2e3e9a9296979cce308da70ffa651bd12d73015c
shayaan-git <mshayaan24@gmail.com>
2026-05-11 12:13:10 +0530

    fix: Protected import case sensitive

--- Frontend/src/app/app.routes.jsx
+++ Frontend/src/app/app.routes.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, Navigate } from "react-router-dom";
 import Login from "../features/auth/pages/Login.jsx";
 import Register from "../features/auth/pages/Register.jsx";
 import Dashboard from "../features/chat/pages/Dashboard.jsx";
-import Protected from "../features/auth/components/protected.jsx";
+import Protected from "../features/auth/components/Protected.jsx";
 
 export const router = createBrowserRouter([
   {
